2eba3c6bbc81ebb6394d90e79cd496b1215fd506
amassarente <massarenteandrea@gmail.com>
2023-11-17 12:18:50 +0100

    add try/catch in main for Exceptions

--- src/com/alten/main/Main.java
+++ src/com/alten/main/Main.java
@@ -11,46 +11,51 @@ import com.alten.util.Zoo;
 
 public class Main {
 
-    public static void main(String[] args) throws WingedAnimalsNotFoundException, TailedAnimalsNotFoundException, EmptySpeciesException {
+    public static void main(String[] args) {
 
-        Zoo zoo = new Zoo();
-        zoo.populateZoo();
+        try {
+            Zoo zoo = new Zoo();
+            zoo.populateZoo();
 
-        Animal highestLion = zoo.findHighestAnimal(Lion.class);
-        Animal shortestLion = zoo.findShortestAnimal(Lion.class);
-        Animal heaviestLion = zoo.findHeaviestAnimal(Lion.class);
-        Animal lighestLion = zoo.findLightestAnimal(Lion.class);
+            Animal highestLion = zoo.findHighestAnimal(Lion.class);
+            Animal shortestLion = zoo.findShortestAnimal(Lion.class);
+            Animal heaviestLion = zoo.findHeaviestAnimal(Lion.class);
+            Animal lighestLion = zoo.findLightestAnimal(Lion.class);
 
-        Animal highestTiger = zoo.findHighestAnimal(Tiger.class);
-        Animal shortestTiger = zoo.findShortestAnimal(Tiger.class);
-        Animal heaviestTiger = zoo.findHeaviestAnimal(Tiger.class);
-        Animal lighestTiger = zoo.findLightestAnimal(Tiger.class);
+            Animal highestTiger = zoo.findHighestAnimal(Tiger.class);
+            Animal shortestTiger = zoo.findShortestAnimal(Tiger.class);
+            Animal heaviestTiger = zoo.findHeaviestAnimal(Tiger.class);
+            Animal lighestTiger = zoo.findLightestAnimal(Tiger.class);
 
-        Animal highestEagle = zoo.findHighestAnimal(Eagle.class);
-        Animal shortestEagle = zoo.findShortestAnimal(Eagle.class);
-        Animal heaviestEagle = zoo.findHeaviestAnimal(Eagle.class);
-        Animal lighestEagle = zoo.findLightestAnimal(Eagle.class);
+            Animal highestEagle = zoo.findHighestAnimal(Eagle.class);
+            Animal shortestEagle = zoo.findShortestAnimal(Eagle.class);
+            Animal heaviestEagle = zoo.findHeaviestAnimal(Eagle.class);
+            Animal lighestEagle = zoo.findLightestAnimal(Eagle.class);
 
-        Animal largestWingspan = zoo.findLargestWingspanAnimal();
+            Animal largestWingspan = zoo.findLargestWingspanAnimal();
 
-        Animal longestTail = zoo.findLongestTailAnimal();
+            Animal longestTail = zoo.findLongestTailAnimal();
 
 
-        System.out.println("Il leone più alto è " + highestLion.getName() + ", il leone più basso è "
-                + shortestLion.getName() + ", il leone più pesante è " + heaviestLion.getName()
-                + ", il leone più leggero è " + lighestLion.getName());
+            System.out.println("The highest lion is " + highestLion.getName() + ", the shortest lion is "
+                    + shortestLion.getName() + ", the heaviest lion is " + heaviestLion.getName()
+                    + ", the lighest lion is " + lighestLion.getName());
 
-        System.out.println("La tigre più alta è " + highestTiger.getName() + ", la tigre più bassa è "
-                + shortestTiger.getName() + ", la tigre più pesante è " + heaviestTiger.getName()
-                + ", la tigre più leggera è " + lighestTiger.getName());
+            System.out.println("The highest tiger is " + highestTiger.getName() + ", the shortest tiger is "
+                    + shortestTiger.getName() + ", the heaviest lion is " + heaviestTiger.getName()
+                    + ", the lighest tiger is " + lighestTiger.getName());
 
-        System.out.println("L'aquila più alta è " + highestEagle.getName() + ", l'aquila più bassa è "
-                + shortestEagle.getName() + ", l'aquila più pesante è " + heaviestEagle.getName()
-                + ", l'aquila più leggera è " + lighestEagle.getName());
+            System.out.println("The highest eagle is " + highestEagle.getName() + ", the shortest eagle is "
+                    + shortestEagle.getName() + ", the heaviest lion is " + heaviestEagle.getName()
+                    + ", the lighest eagle is " + lighestEagle.getName());
 
-        System.out.println("L'animale con l'apertura alare maggiore è " + largestWingspan.getName());
+            System.out.println("The animal with the largest wingspan is " + largestWingspan.getName());
 
-        System.out.println("L'animale con la coda più lunga è " + longestTail.getName());
+            System.out.println("The animal with the longest tail is " + longestTail.getName());
+
+        } catch (EmptySpeciesException | WingedAnimalsNotFoundException | TailedAnimalsNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/com/alten/util/Zoo.java
+++ src/com/alten/util/Zoo.java
@@ -6,79 +6,79 @@ import com.alten.exception.WingedAnimalsNotFoundException;
 import com.alten.model.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Zoo {
 
-    private List<Animal> animals;
+    private Map<Class<?extends Animal>, List<Animal>> animalsMap;
 
     public Zoo() {
-        this.animals = new ArrayList<>();
+        this.animalsMap = new HashMap<>();
     }
 
     public void addAnimal(Animal animal) {
-        animals.add(animal);
+        Class<?extends Animal> species = animal.getClass();
+        animalsMap.computeIfAbsent(species, k -> new ArrayList<>()).add(animal);
     }
 
     public void populateZoo() {
         addAnimal(new Lion("Simba", "Zebra", 2, LocalDate.of(2023, 10, 9), 1.50f, 100f, 0.50f));
-        addAnimal(new Lion("Mufasa", "Bufalo", 8, LocalDate.of(2019, 10, 10), 2.50f, 200f, 1.00f));
+        addAnimal(new Lion("Mufasa", "Buffalo", 8, LocalDate.of(2019, 10, 10), 2.50f, 200f, 1.00f));
         addAnimal(new Lion("Nala", "Zebra", 4, LocalDate.of(2018, 10, 7), 1.70f, 155.5f, 0.75f));
 
-        addAnimal(new Tiger("Diego", "Gazzella", 1, LocalDate.of(2023, 10, 10), 1.40f, 50f, 0.20f));
-        addAnimal(new Tiger("Red", "Antilope", 5, LocalDate.of(2019, 10, 6), 1.80f, 150f, 0.50f));
-        addAnimal(new Tiger("Black", "Gazzella", 7, LocalDate.of(2020, 10, 10), 2.00f, 270f, 0.80f));
+        addAnimal(new Tiger("Diego", "Gazzelle", 1, LocalDate.of(2023, 10, 10), 1.40f, 50f, 0.20f));
+        addAnimal(new Tiger("Red", "Antelope", 5, LocalDate.of(2019, 10, 6), 1.80f, 150f, 0.50f));
+        addAnimal(new Tiger("Black", "Antelope", 7, LocalDate.of(2020, 10, 10), 2.00f, 270f, 0.80f));
 
-        addAnimal(new Eagle("Olympia", "Coniglio", 3, LocalDate.of(2019, 10, 5), 0.75f, 3f, 1.80f));
-        addAnimal(new Eagle("Sky", "Coniglio", 3, LocalDate.of(2018, 10, 3), 0.50f, 4f, 1.60f));
-        addAnimal(new Eagle("Blue", "Trota", 4, LocalDate.of(2019, 10, 10), 0.60f, 2f, 2.15f));
+        addAnimal(new Eagle("Olympia", "Rabbit", 3, LocalDate.of(2019, 10, 5), 0.75f, 3f, 1.80f));
+        addAnimal(new Eagle("Sky", "Rabbit", 3, LocalDate.of(2018, 10, 3), 0.50f, 4f, 1.60f));
+        addAnimal(new Eagle("Blue", "Fish", 4, LocalDate.of(2019, 10, 10), 0.60f, 2f, 2.15f));
     }
 
 
-    private List<Animal> findAllAnimalsBySpecies(List<Animal> animals, Class<? extends Animal> animalType) {
-        return animals.stream().filter(animalType::isInstance).collect(Collectors.toList());
+    private List<Animal> findAllAnimalsBySpecies(Class<? extends Animal> animalType) {
+        return animalsMap.getOrDefault(animalType, Collections.emptyList());
     }
 
     public Animal findHighestAnimal(Class<? extends Animal> species) throws EmptySpeciesException {
-        List<Animal> matchingAnimals = findAllAnimalsBySpecies(animals, species);
+        List<Animal> matchingAnimals = findAllAnimalsBySpecies(species);
 
         return matchingAnimals.stream().max(Comparator.comparing(Animal::getHeight))
                 .orElseThrow(() -> new EmptySpeciesException("There are no animals for the selected species"));
     }
 
     public Animal findShortestAnimal(Class<? extends Animal> species) throws EmptySpeciesException {
-        List<Animal> matchingAnimals = findAllAnimalsBySpecies(animals, species);
+        List<Animal> matchingAnimals = findAllAnimalsBySpecies(species);
 
         return matchingAnimals.stream().min(Comparator.comparing(Animal::getHeight))
                 .orElseThrow(() -> new EmptySpeciesException("There are no animals for the selected species"));
     }
 
     public Animal findHeaviestAnimal(Class<? extends Animal> species) throws EmptySpeciesException {
-        List<Animal> matchingAnimals = findAllAnimalsBySpecies(animals, species);
+        List<Animal> matchingAnimals = findAllAnimalsBySpecies(species);
 
         return matchingAnimals.stream().max(Comparator.comparing(Animal::getWeight))
                 .orElseThrow(() -> new EmptySpeciesException("There are no animals for the selected species"));
     }
 
     public Animal findLightestAnimal(Class<? extends Animal> species) throws EmptySpeciesException {
-        List<Animal> matchingAnimals = findAllAnimalsBySpecies(animals, species);
+        List<Animal> matchingAnimals = findAllAnimalsBySpecies(species);
 
         return matchingAnimals.stream().min(Comparator.comparing(Animal::getWeight))
                 .orElseThrow(() -> new EmptySpeciesException("There are no animals for the selected species"));
     }
 
     public Animal findLargestWingspanAnimal() throws WingedAnimalsNotFoundException {
-        return animals.stream().filter(animal -> animal instanceof WingedAnimal)
+        return animalsMap.values().stream().flatMap(List::stream)
+                .filter(animal -> animal instanceof WingedAnimal)
                 .map(animal -> (WingedAnimal) animal)
                 .max(Comparator.comparing(WingedAnimal::getWingspan))
                 .orElseThrow(() -> new WingedAnimalsNotFoundException("There are no animals with wing in the zoo"));
     }
 
     public Animal findLongestTailAnimal() throws TailedAnimalsNotFoundException {
-        return animals.stream().filter(animal -> animal instanceof TailedAnimal)
+        return animalsMap.values().stream().flatMap(List::stream)
+                .filter(animal -> animal instanceof TailedAnimal)
                 .map(animal -> (TailedAnimal) animal)
                 .max(Comparator.comparing(TailedAnimal::getTailLenght))
                 .orElseThrow(() -> new TailedAnimalsNotFoundException("There are no animals with tail in the zoo"));
